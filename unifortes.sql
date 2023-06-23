e4f602cae3138043b00ed7df68408c6920647fbb
PorousBunion8 <126472498+PorousBunion8@users.noreply.github.com>
2023-06-23 14:28:21 -0300

    Update unifortes.sql

--- unifortes.sql
+++ unifortes.sql
@@ -1,17 +1,28 @@
-
+----------------------------------------------------------------------------------
+-- Criação das tabela loja
+-------------------------------------------------------------------------------
 CREATE TABLE public.Loja (
                 Recompensa_id NUMERIC(3) NOT NULL,
                 Quantidade NUMERIC(10) NOT NULL,
                 Valor NUMERIC(6,2) NOT NULL,
                 Recompensas VARCHAR(255) NOT NULL,
+  --
+--  chave primaria
+----------------------------------------------------------------------------------
                 CONSTRAINT id_recompensa PRIMARY KEY (Recompensa_id)
 );
+----------------------------------------------------------------------------------
+-- Comentarios da tabela
+-------------------------------------------------------------------------------
 COMMENT ON TABLE public.Loja IS 'Tabela referente ás recompensas obtidas';
 COMMENT ON COLUMN public.Loja.Recompensa_id IS 'Identificador unico das recompensas';
 COMMENT ON COLUMN public.Loja.Quantidade IS 'Quantidade de recompensas de cada produto';
 COMMENT ON COLUMN public.Loja.Valor IS 'Valor da recompensa';
 COMMENT ON COLUMN public.Loja.Recompensas IS 'Nome das recompensas';
 
+----------------------------------------------------------------------------------
+-- Criação das tabela funcionarios
+-------------------------------------------------------------------------------
 
 CREATE TABLE public.Funcionrios (
                 cpf CHAR NOT NULL,
@@ -23,8 +34,14 @@ CREATE TABLE public.Funcionrios (
                 Pontos NUMERIC(12),
                 Sexo VARCHAR(1) NOT NULL,
                 Certificados VARCHAR(255),
+    --
+--  chave primaria
+----------------------------------------------------------------------------------
                 CONSTRAINT cpf PRIMARY KEY (cpf)
 );
+----------------------------------------------------------------------------------
+-- Comentarios da tabela
+-------------------------------------------------------------------------------
 COMMENT ON TABLE public.Funcionrios IS 'Tabela que representa os funcionários usuários do site';
 COMMENT ON COLUMN public.Funcionrios.cpf IS 'CPF do funcionario';
 COMMENT ON COLUMN public.Funcionrios.Nome IS 'Nome do funcionário';
@@ -36,47 +53,76 @@ COMMENT ON COLUMN public.Funcionrios.Pontos IS 'Pontos obtidos pelo funcionário
 COMMENT ON COLUMN public.Funcionrios.Sexo IS 'Sexo do funcionário';
 COMMENT ON COLUMN public.Funcionrios.Certificados IS 'Certificado obtido pelo funcionário ao completar um curso';
 
+----------------------------------------------------------------------------------
+-- Criação das tabela curso
+-------------------------------------------------------------------------------
 
 CREATE TABLE public.Curso (
                 Curso_id NUMERIC(2) NOT NULL,
                 Duracao VARCHAR(255) NOT NULL,
                 curso_nome VARCHAR(255) NOT NULL,
                 cpf CHAR NOT NULL,
+    --
+--  chave primaria
+----------------------------------------------------------------------------------
                 CONSTRAINT curso_id PRIMARY KEY (Curso_id)
 );
+----------------------------------------------------------------------------------
+-- Comentarios da tabela
+-------------------------------------------------------------------------------
 COMMENT ON TABLE public.Curso IS 'Tabela referente aos cursos disponiveis no site';
 COMMENT ON COLUMN public.Curso.Curso_id IS 'Identificador unico do curso';
 COMMENT ON COLUMN public.Curso.Duracao IS 'Duracao de um curso';
 COMMENT ON COLUMN public.Curso.curso_nome IS 'nome do curso';
 COMMENT ON COLUMN public.Curso.cpf IS 'CPF do funcionario';
 
+----------------------------------------------------------------------------------
+-- Criação das tabela jogos
+-------------------------------------------------------------------------------
 
 CREATE TABLE public.Jogos (
                 Jogo_id NUMERIC(2) NOT NULL,
                 nome_jogo VARCHAR(255) NOT NULL,
                 Premio NUMERIC(4) NOT NULL,
                 Curso_id NUMERIC(2) NOT NULL,
+    --
+--  chave primaria
+----------------------------------------------------------------------------------
                 CONSTRAINT jogo_id PRIMARY KEY (Jogo_id)
 );
+----------------------------------------------------------------------------------
+-- Comentarios da tabela
+-------------------------------------------------------------------------------
 COMMENT ON TABLE public.Jogos IS 'Tabela referente aos jogos do site';
 COMMENT ON COLUMN public.Jogos.Jogo_id IS 'Identificador unico do jogo';
 COMMENT ON COLUMN public.Jogos.nome_jogo IS 'nome do jogo';
 COMMENT ON COLUMN public.Jogos.Premio IS 'Premio do jogo em pontos';
 COMMENT ON COLUMN public.Jogos.Curso_id IS 'Identificador unico do curso';
 
+----------------------------------------------------------------------------------
+-- Criação das tabela pontos de resgate
+-------------------------------------------------------------------------------
 
 CREATE TABLE public.Pontos_de_resgate (
                 Loja_Recompensa_id NUMERIC(3) NOT NULL,
                 cpf CHAR NOT NULL,
                 Pontos NUMERIC(12),
+    --
+--  chave primaria
+----------------------------------------------------------------------------------
                 CONSTRAINT recompensa_id PRIMARY KEY (Loja_Recompensa_id, cpf)
 );
+----------------------------------------------------------------------------------
+-- Comentarios da tabela
+-------------------------------------------------------------------------------
 COMMENT ON TABLE public.Pontos_de_resgate IS 'tabela referente aos pontos de resgate para loja';
 COMMENT ON COLUMN public.Pontos_de_resgate.Loja_Recompensa_id IS 'Identificador unico das recompensas';
 COMMENT ON COLUMN public.Pontos_de_resgate.cpf IS 'CPF do funcionário';
 COMMENT ON COLUMN public.Pontos_de_resgate.Pontos IS 'Pontos para resgate das recompensas';
 
-
+----------------------------------------------------------------------------------
+-- Criação dos Check da tabela
+-------------------------------------------------------------------------------
 ALTER TABLE public.Pontos_de_resgate ADD CONSTRAINT loja_pontos_de_resgate_fk
 FOREIGN KEY (Loja_Recompensa_id)
 REFERENCES public.Loja (Recompensa_id)
